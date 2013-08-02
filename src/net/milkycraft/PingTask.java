0fc992b5f3e2ef8a821932099dee0b9ac6273da8
Nick Porillo <milkywayz@mail.com>
2013-08-02 02:38:33 -0400

    Ping!

---
+++ src/net/milkycraft/PingTask.java
@@ -0,0 +1,45 @@
+package net.milkycraft;
+
+import java.io.DataInputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.TimerTask;
+
+import ch.jamiete.mcping.MinecraftPing;
+import ch.jamiete.mcping.MinecraftPingReply;
+
+public class PingTask extends TimerTask {
+
+	private MinecraftPing ping = new MinecraftPing();
+	private Config c;
+
+	public PingTask(Config config) {
+		this.c = config;
+	}
+
+	@Override
+	public void run() {
+		try {
+			if (c.getPingMode().equalsIgnoreCase("Custom")) {
+				final Socket socket = new Socket();
+				socket.connect(new InetSocketAddress(c.getAddress(), c.getPort()), 3000);
+				final DataInputStream in = new DataInputStream(socket.getInputStream());
+				int count = in.read();
+				Utility.setTray(count);
+			} else {
+				MinecraftPingReply mpr = ping.getPing(c.getAddress(), c.getPort());
+				Utility.setTray(mpr.getOnlinePlayers());
+			}
+
+		} catch (Exception e) {
+			Utility.log(e);
+			try {
+				Utility.setTray(c.getPreload() + 1);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+				System.exit(0);
+			}
+		}
+	}
+
+}
